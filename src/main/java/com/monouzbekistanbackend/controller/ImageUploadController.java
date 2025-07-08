5282764dd304f0a06b876b503a9c1cb56c171ebe
Elyor <atabayev.elyor0219@gmail.com>
2025-07-08 22:09:36 +0500

    Image upload updated

--- src/main/java/com/monouzbekistanbackend/controller/ImageUploadController.java
+++ src/main/java/com/monouzbekistanbackend/controller/ImageUploadController.java
@@ -12,12 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -37,24 +33,18 @@ public class ImageUploadController {
     }
 
     @PostMapping(value = "upload-image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<ProductPhotoResponse> uploadImage(@RequestParam("file") MultipartFile file,
+    public ResponseEntity<ProductPhotoResponse> uploadImage(@RequestParam("imageUrl") String imageUrl,
                                                             @RequestParam("productId") String productId,
                                                             @RequestParam("color") String color ) throws IOException {
         Product product = productRepository.findByProductId(productId)
                 .orElseThrow(() -> new RuntimeException("Product not found"));
-
-        String filaName = UUID.randomUUID() + "_" + file.getOriginalFilename();
-        String UPLOADED_FILE = "C:/Users/user/Pictures/MonoUzbBotPhotos/";
-        Path path = Paths.get(UPLOADED_FILE + filaName);
-        Files.createDirectories(path.getParent());
-        Files.write(path, file.getBytes());
 
         ProductPhoto productPhoto = new ProductPhoto();
         productPhoto.setProductPhotoId(UUID.randomUUID());
         productPhoto.setMain(false);
         productPhoto.setProduct(product);
         productPhoto.setColor(color);
-        productPhoto.setUrl("/images/" + filaName);
+        productPhoto.setUrl(imageUrl);
         productPhoto.setCreatedAt(LocalDateTime.now());
         productImageRepository.save(productPhoto);
 
