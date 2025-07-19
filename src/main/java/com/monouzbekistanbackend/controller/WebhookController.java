84e7268937d29fc31f315f1cb5dd0d1fa929a9cd
Elyor <atabayev.elyor0219@gmail.com>
2025-07-19 10:12:52 +0500

    webhook updated

--- src/main/java/com/monouzbekistanbackend/controller/WebhookController.java
+++ src/main/java/com/monouzbekistanbackend/controller/WebhookController.java
@@ -1,22 +1,26 @@
 package com.monouzbekistanbackend.controller;
 
+import com.monouzbekistanbackend.config.MonoUzbBot;
 import com.monouzbekistanbackend.service.TelegramBotService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @RestController
 public class WebhookController {
 
     private final TelegramBotService telegramBotService;
+    private final MonoUzbBot monoUzbBot;
 
-    public WebhookController(TelegramBotService telegramBotService) {
+    public WebhookController(TelegramBotService telegramBotService, MonoUzbBot monoUzbBot) {
         this.telegramBotService = telegramBotService;
+        this.monoUzbBot = monoUzbBot;
     }
 
     @PostMapping("/webhook")
-    public void onUpdateReceived(@RequestBody Update update) {
-        telegramBotService.handleIncomingMessage(update.getMessage());
+    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+        return monoUzbBot.onWebhookUpdateReceived(update);
     }
 }
